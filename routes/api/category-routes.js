a439b368dd6eb9f343a871f5382c6a88290057b1
Timothy Lai <timothylai1121@gmail.com>
2023-03-24 20:48:36 -0400

    working on category-routes.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,28 +1,104 @@
+// category routes //
+
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
+// adding async to the function instead of await //
 // The `/api/categories` endpoint
-
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+// TODO
+// find all categories
+// const categories
+// be sure to include its associated Products
+router.get('/', async (req, res) => {
+  try {
+    const categories = await Category.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(categories);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+//
+// TODO
+// find one category by its `id` value
+// const category
+// be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  try {
+    const category = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+
+    if (!category) {
+      res.status(404).json({message: 'No category found with this id'});
+  }
+  res.status(200).json(category);
+} catch (err) {
+  res.status(500).json(err);
+}
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+
+//
+// TODO
+// create a new category
+// const newCategory
+router.post('/', async (req, res) => {
+  try {
+    const newCategory = await Category.create(req.body);
+    res.status(200).json(newCategory);
+  } catch (err) {
+    res.status(500).jsonn(err);
+  }
+ });
+
+
+//
+// TODO
+// update a category by its `id` value
+// const updatedCategory
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedCategory = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!updatedCategory[0]) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+
+    res.status(200).json({ message: 'Category updated successfully!' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
-});
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+//
+// TODO
+// delete a category by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const deletedCategory = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!deletedCategory) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+
+    res.status(200).json({ message: 'Category deleted successfully!' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,23 @@
+// npm i express sequelize mysql2 //
+// npm i dotenv //
+// npm i chalk //
+
+// import express from 'express';
 const express = require('express');
+// import routes from './routes';
 const routes = require('./routes');
 // import sequelize connection
 
+// express server
 const app = express();
+// set up port
 const PORT = process.env.PORT || 3001;
 
+
+// middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+// turn on routes
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
